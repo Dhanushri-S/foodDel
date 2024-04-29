e598f06f4a5b6666a5dc51de4e483a83706a63bb
Dhanushri s <90515404+Dhanushri-S@users.noreply.github.com>
2024-04-29 22:44:54 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:"/foodDel/"
-  plugins: [react()],
+  plugins: [
+    { enforce: 'pre', ...mdx() },
+    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+  ],
 })

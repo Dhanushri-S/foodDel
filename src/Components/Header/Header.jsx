64ae8dc0134116e0297f4b07224f439cddd3ec14
Dhanushri s <90515404+Dhanushri-S@users.noreply.github.com>
2024-04-30 22:57:10 +0530

    Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
+import header_img from './header_img.png'
 
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' style={{ 
+      backgroundImage: `url( ${header_img} )` }}>
         <div className='header-contents'>
             <h2>Order your favourite food here</h2>
             <p>Choose from diverse menu featuring a delectable array of dishes.
